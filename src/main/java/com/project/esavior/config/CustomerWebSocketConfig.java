a80a96c22934b060601b735ad1eb27dbacffdf7d
duongthutb9x <duongthutb941@gmail.com>
2024-09-30 14:03:11 +0700

    ok

--- src/main/java/com/project/esavior/config/CustomerWebSocketConfig.java
+++ src/main/java/com/project/esavior/config/CustomerWebSocketConfig.java
@@ -1,7 +1,6 @@
 package com.project.esavior.config;
 
 import com.project.esavior.websocket.websocket.CustomerWebSocketHandler;
-import com.project.esavior.websocket.websocket.DriverWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -9,14 +8,10 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 @Configuration
 @EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+public class CustomerWebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        // Đăng ký WebSocket endpoint cho tài xế
-        registry.addHandler(new DriverWebSocketHandler(), "/ws/driver")
-                .setAllowedOriginPatterns("*");
-
         // Đăng ký WebSocket endpoint cho khách hàng
         registry.addHandler(new CustomerWebSocketHandler(), "/ws/customer")
                 .setAllowedOriginPatterns("*");
